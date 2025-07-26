d45f2b366a5f7a7c94ce8b385be28fda01ee0b76
Utsav <utsavc317@gmail.com>
2025-07-26 22:27:43 +0530

    rebuild application

--- dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting YouTube Music Desktop in Development Mode...
-echo.
-echo This will enable detailed logging and developer tools.
-echo.
-npm run dev
-pause
--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting YouTube Music Desktop...
-echo.
-echo Make sure you have Node.js installed on your system.
-echo If you encounter any issues, run 'npm install' first.
-echo.
-npm start
-pause
